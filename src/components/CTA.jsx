4dd9b27699c33031fc225d04c732074c7e659235
VB <vb_2.0@pm.me>
2023-01-27 11:38:26 -0500

    Header section updated

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,10 +3,12 @@ import CV from "../assets/cv.png";
 function CTA() {
   return (
     <div className="cta">
-      <a href={CV} download>
+      <a href={CV} download className="btn">
         Download CV
       </a>
-      <a href="#contact">Messege me</a>
+      <a href="#contact" className="btn btn-primary">
+        Messege me
+      </a>
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,22 +7,24 @@ import { SiLinkedin } from "react-icons/si";
 function Header() {
   return (
     <header id="header">
-      <h5>Hello, I am</h5>
-      <h1>Vitaliy Bernatskyy</h1>
-      <h5>Fullstack Developer</h5>
-      <CTA />
-      <img
-        src={ME}
-        alt="me"
-        width={300}
-        style={{ borderRadius: 200, marginTop: 7 * 16 }}
-      />
-      <div className="header__socials">
-        <a href="https://github.com/vyber7">
-          <SiGithub />
-        </a>
-        <a href="https://linkedin.com/">
-          <SiLinkedin />
+      <div className="container header__container">
+        <h5>Hello, I am</h5>
+        <h1>Vitaliy Bernatskyy</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <div className="header__socials">
+          <a href="https://github.com/vyber7">
+            <SiGithub />
+          </a>
+          <a href="https://linkedin.com/">
+            <SiLinkedin />
+          </a>
+        </div>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
         </a>
       </div>
     </header>
