9da858121fdfe5bd11359290b7ca5664af16a808
VB <vb_2.0@pm.me>
2023-02-06 12:26:59 -0500

    Experience section updated

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,10 +1,79 @@
 import "../styles/experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 function Experience() {
   return (
     <section id="experience">
       <h5>What skills I have</h5>
-      <h2>My Esperience</h2>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__card">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="experience__card">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NodeJS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NextJS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>SQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+            <div className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
