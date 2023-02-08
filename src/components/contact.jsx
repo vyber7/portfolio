dde6d347a729be18cb2f3396e433233f25f77ee4
VB <vb_2.0@pm.me>
2023-02-08 11:09:22 -0500

    HTML for contact page is done

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,52 @@
 import "../styles/contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
 
 function Contact() {
-  return <section id="contact">contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>vb_2.0@pm.me</h5>
+            <a href="mailto:vb_2.0@pm.me" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>305-335-0473</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+13053350473"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" placeholder="Name" name="name" required />
+          <input type="email" placeholder="Email" name="email" required />
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows="7"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
