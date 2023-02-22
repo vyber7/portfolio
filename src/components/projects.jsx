8b6c718f00ec7632b7ccee7abe30aeabaed2ec01
VB <vb_2.0@pm.me>
2023-02-22 11:19:31 -0500

    Projects section updated
    Thumbnails added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,43 @@
 import "../styles/projects.css";
+import Calculator from "../assets/calculator.png";
+import DrumMachine from "../assets/drum_machine.png";
+import Clock from "../assets/25_plus_5_clock.png";
+import HeatMap from "../assets/heat_map.png";
+import TreeMap from "../assets/tree_map.png";
+import RandomQuotes from "../assets/random_quotes.png";
+
+const data = [
+  {
+    title: "Calculator",
+    img: Calculator,
+    demo: "https://codepen.io/vyber7/full/ExVyQeN",
+  },
+  {
+    title: "Drum Machine",
+    img: DrumMachine,
+    demo: "https://codepen.io/vyber7/full/wvKKjox",
+  },
+  {
+    title: "25 + 5 Clock",
+    img: Clock,
+    demo: "https://codepen.io/vyber7/full/QWjqZMX",
+  },
+  {
+    title: "Heat Map",
+    img: HeatMap,
+    demo: "https://codepen.io/vyber7/full/xxZjBVr",
+  },
+  {
+    title: "Tree Map",
+    img: TreeMap,
+    demo: "https://codepen.io/vyber7/full/qBZjRyg",
+  },
+  {
+    title: "Random Quotes",
+    img: RandomQuotes,
+    demo: "https://codepen.io/vyber7/full/VwLOGze",
+  },
+];
 
 function Projects() {
   return (
@@ -7,90 +46,24 @@ function Projects() {
       <h2>Projects</h2>
 
       <div className="container projects__container">
-        <article className="projects__item">
-          <div className="projects__item-img">
-            <img src="https://picsum.photos/700?random=1" alt="img"></img>
-          </div>
-          <h3>Title</h3>
-          <a
-            href="https://codepen.io/vyber7"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Demo
-          </a>
-        </article>
-        <article className="projects__item">
-          <div className="projects__item-img">
-            <img src="https://picsum.photos/700?random=1" alt="img"></img>
-          </div>
-          <h3>Title</h3>
-          <a
-            href="https://codepen.io/vyber7"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Demo
-          </a>
-        </article>
-        <article className="projects__item">
-          <div className="projects__item-img">
-            <img src="https://picsum.photos/700?random=1" alt="img"></img>
-          </div>
-          <h3>Title</h3>
-          <a
-            href="https://codepen.io/vyber7"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Demo
-          </a>
-        </article>
-        <article className="projects__item">
-          <div className="projects__item-img">
-            <img src="https://picsum.photos/700?random=1" alt="img"></img>
-          </div>
-          <h3>Title</h3>
-          <a
-            href="https://codepen.io/vyber7"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Demo
-          </a>
-        </article>
-        <article className="projects__item">
-          <div className="projects__item-img">
-            <img src="https://picsum.photos/700?random=1" alt="img"></img>
-          </div>
-          <h3>Title</h3>
-          <a
-            href="https://codepen.io/vyber7"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Demo
-          </a>
-        </article>
-        <article className="projects__item">
-          <div className="projects__item-img">
-            <img src="https://picsum.photos/700?random=1" alt="img"></img>
-          </div>
-          <h3>Title</h3>
-          <a
-            href="https://codepen.io/vyber7"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Demo
-          </a>
-        </article>
+        {data.map((item) => {
+          return (
+            <article className="projects__item">
+              <div className="projects__item-img">
+                <img src={item.img} alt="img"></img>
+              </div>
+              <h3>{item.title}</h3>
+              <a
+                href={item.demo}
+                className="btn"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Demo
+              </a>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
