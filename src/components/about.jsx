5adc0a5e7126625292609d6e5dedf788eff1366b
VB <vb_2.0@pm.me>
2023-04-18 12:24:15 -0400

    Styles updated

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,24 +6,22 @@ function About() {
   return (
     <section id="about">
       <div className="title">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+          />
+        </svg>
         <h5>Get To Know Me</h5>
-        <h2>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-          </svg>
-          About Me
-        </h2>
+        <h2>About Me</h2>
       </div>
 
       <div className="container about__container">
--- src/components/certifications.jsx
+++ src/components/certifications.jsx
@@ -6,7 +6,7 @@ import FrontEndDevelopmentLibraries from "../assets/front_end_development_librar
 import DataVisualization from "../assets/data_visualization.png";
 import BackEndDevelopmentAndAPIs from "../assets/back_end_development_and_apis.png";
 import { register } from "swiper/element/bundle";
-//import Swiper from "swiper/bundle";
+
 register();
 
 const data = [
@@ -41,47 +41,46 @@ function Certs() {
   return (
     <section id="certifications">
       <div className="title">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+          />
+        </svg>{" "}
         <h5>My Certifications</h5>
-        <h2>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
-            />
-          </svg>
-          Certifications
-        </h2>
+        <h2>Certifications</h2>
       </div>
-
-      <swiper-container
-        pagination="true"
-        slides-per-view="1"
-        speed="400"
-        space-between="100"
-        loop="true"
-        autoplay="true"
-        disable-on-interaction="false"
-        class="container certifications__container"
-      >
-        {data.map((item, i) => {
-          return (
-            <swiper-slide class="certifications__item" key={i}>
-              <a href={item.demo} target="_blank" rel="noreferrer">
-                <img src={item.img} alt="img"></img>
-                <h3>{item.title}</h3>
-              </a>
-            </swiper-slide>
-          );
-        })}
-      </swiper-container>
+      <div className="container certifications__container">
+        <swiper-container
+          pagination="true"
+          slides-per-view="1"
+          speed="400"
+          space-between="100"
+          loop="true"
+          autoplay="true"
+          disable-on-interaction="false"
+          class="certifications__slider"
+        >
+          {data.map((item, i) => {
+            return (
+              <swiper-slide class="certifications__item" key={i}>
+                <a href={item.demo} target="_blank" rel="noreferrer">
+                  <img src={item.img} alt="img"></img>
+                  <h3>{item.title}</h3>
+                </a>
+              </swiper-slide>
+            );
+          })}
+        </swiper-container>
+      </div>
     </section>
   );
 }
