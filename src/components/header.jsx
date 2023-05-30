d078589ba8a13b6e13b866eb4a987e592721e813
VB <vb_2.0@pm.me>
2023-05-30 11:08:28 -0400

    picture uodated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import "../styles/header.css";
 import CTA from "./CTA";
-import ME from "../assets/me.png";
+import ME from "../assets/me.jpg";
 import { SiGithub } from "react-icons/si";
 import { SiLinkedin } from "react-icons/si";
 import { SiCodepen } from "react-icons/si";
